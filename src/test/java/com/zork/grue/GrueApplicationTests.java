b31de205e057d8211c25674d501251bd21498999
Ronald Chaplin <chaplin.ronald84@gmail.com>
2020-11-04 17:45:23 -0500

    Commit

--- src/test/java/com/zork/grue/GrueApplicationTests.java
+++ src/test/java/com/zork/grue/GrueApplicationTests.java
@@ -1,13 +1,34 @@
 package com.zork.grue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
 class GrueApplicationTests {
+	private WebDriver driver;
+
+	@BeforeEach
+	void setUp(){
+		System.setProperty("webdriver.chrome.driver", "d:/selenium/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://zorkherokutest.herokuapp.com/");
+	}
 
 	@Test
 	void contextLoads() {
+		assertEquals("https://zorkherokutest.herokuapp.com/", driver.getCurrentUrl());
+	}
+
+	@Test
+	void turnOnLightsTest(){
+		WebElement button = driver.findElement(By.xpath("/html/body/form[1]/button)"));
+		button.click();
+		assertEquals("https://zorkherokutest.herokuapp.com/lights?",driver.getCurrentUrl());
 	}
 
 }
